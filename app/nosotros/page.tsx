5e838065c9c498b595e53346f74295821d043641
BenjaminDev701 <learncodigo777@gmail.com>
2026-09-15 12:43:31 -0600

    Configuracion tipografica

---
+++ app/nosotros/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Meeti - Nosotros"
+}
+
+export default function NosotrosPage() {
+    return (
+        <div>
+            <h1>NosotrosPage</h1>
+        </div>
+    );
+}
