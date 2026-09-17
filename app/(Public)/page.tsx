55d5987b0bfc4090aa85e9c193ba4b710648aa68
BenjaminDev701 <learncodigo777@gmail.com>
2026-09-17 00:28:46 -0600

    Feat(SEO):Creacion de Helper para el titulo en los metadatos

--- app/(Public)/page.tsx
+++ app/(Public)/page.tsx
@@ -1,10 +1,11 @@
 
 import Hero from "@/src/shared/components/ui/Hero";
+import { generatePageTitle } from "@/src/shared/utils/metadata";
 import { Metadata } from "next";
 
 //*siempre se debe exportar esto, esto es el SEO de la pagina
 export const metadata: Metadata = {
-  title: "Meeti - Inicio"
+  title: generatePageTitle("Inicio")
 }
 
 export default function Home() {
