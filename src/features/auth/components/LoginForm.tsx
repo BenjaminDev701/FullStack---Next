39c3b1fa0cee536b749a9c5b12ce703272bc0302
BenjaminDev701 <learncodigo777@gmail.com>
2026-09-18 23:16:20 -0600

    Feat:Creacion de label Re-utilizable

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,15 +1,16 @@
 "use client"
 
 import Form from "@/src/shared/components/forms/Form";
+import FormLabel from "@/src/shared/components/forms/FormLabel";
 
 export default function LoginForm() {
     return (
         <Form >
-            <label htmlFor="email">E-mail</label>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
             <input type="email" id="email" placeholder="correo@correo.com"
                 className="border border-slate-200 w-full p-2" />
 
-            <label htmlFor="password">Contraseña</label>
+            <FormLabel htmlFor="password">Contraseña</FormLabel>
             <input type="password" id="password" placeholder="**********"
                 className="border border-slate-200 w-full p-2" />
 
--- src/shared/components/forms/FormLabel.tsx
+++ src/shared/components/forms/FormLabel.tsx
@@ -1,7 +1,12 @@
-export default function FormLabel() {
+import clsx from "clsx";
+import { LabelHTMLAttributes } from "react";
+
+type Props = LabelHTMLAttributes<HTMLLabelElement>
+
+export default function FormLabel({ children, htmlFor, className, ...props }: Props) {
+
+
     return (
-        <div>
-            <h1>Formlabel</h1>
-        </div>
+        <label htmlFor={htmlFor} className={clsx("font-semibold", className)}{...props}>{children}</label>
     );
 }
