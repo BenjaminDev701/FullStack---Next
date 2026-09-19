5993aad6eb370a55351d859eec72ed6fe4448c18
BenjaminDev701 <learncodigo777@gmail.com>
2026-09-18 23:01:10 -0600

    Feat:Permitir al Componente recibir clases externas

--- src/shared/components/forms/Form.tsx
+++ src/shared/components/forms/Form.tsx
@@ -1,11 +1,16 @@
 import { FormHTMLAttributes } from "react";
+import clsx from "clsx";
 
 type Props = FormHTMLAttributes<HTMLFormElement>
 
-export default function Form(props: Props) {
+export default function Form({ children, className, ...props }: Props) {
+
+
     return (
-        <form className="mt-10 space-y-3 p-5 shadow-2xl">
-            {props.children}
-        </form>
+        //*prop toma todas las caracteristicas de un formulario html y las pasa al componente
+        <form className={clsx("mt-10 space-y-3 p-5", className)} {...props} >
+            {/**children el componente que toma lo que esta dentro de form */}
+            {children}
+        </form >
     );
 }
