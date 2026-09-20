e555c52c583b748d3941bbc9a7f17be533cb7b34
BenjaminDev701 <learncodigo777@gmail.com>
2026-09-20 15:39:42 -0600

    Feat:Añadiendo Router entre Paginas(CreateAccount, ForgotPassword, Login)

--- app/auth/create-account/page.tsx
+++ app/auth/create-account/page.tsx
@@ -2,6 +2,7 @@ import RegisterForm from "@/src/features/auth/components/RegisterForm";
 import Heading from "@/src/shared/components/tipografia/Heading";
 import { generatePageTitle } from "@/src/shared/utils/metadata";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: generatePageTitle("Crear Cuenta")
@@ -12,6 +13,12 @@ export default function RegisterPage() {
         <>
             <Heading level={1}>Crear Cuenta</Heading>
             <RegisterForm />
+
+            <nav className="mt-20 flex justify-between">
+                <Link href={"/auth/login"} className="font-bold">Iniciar Sesión</Link>
+
+                <Link href={"/auth/forgot-password"}>Olvide Contraseña</Link>
+            </nav>
         </>
     )
 }   
--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -2,6 +2,7 @@ import ForgotPasswordForm from "@/src/features/auth/components/ForgotPasswordFor
 import Heading from "@/src/shared/components/tipografia/Heading";
 import { generatePageTitle } from "@/src/shared/utils/metadata";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: generatePageTitle("Reestablecer Contraseña")
@@ -14,6 +15,11 @@ export default function ForgotPasswordPage() {
 
             <Heading level={2} >Recupera tu acceso a Meeti</Heading>
             <ForgotPasswordForm />
+            <nav className="mt-20 flex justify-between">
+                <Link href={"/auth/login"} className="font-bold">Iniciar Sesión</Link>
+
+                <Link href={"/auth/create-account"}>Crear Cuenta</Link>
+            </nav>
         </>
     );
 }
